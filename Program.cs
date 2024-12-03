8acb9418655a255c791f721da7baf1cfa750392b
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-12-03 06:53:06 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//*****************************************************************************
+//** 2109. Adding Spaces to a String    leetcode                             **
+//*****************************************************************************
+
+char* addSpaces(char* s, int* spaces, int spacesSize)
+{
+    int len = strlen(s);
+    char* result = (char*)malloc((len + spacesSize + 1) * sizeof(char));
+    int resIndex = 0;
+    int spaceIndex = 0; 
+
+    for (int i = 0; i < len; i++)
+    {
+        if (spaceIndex < spacesSize && spaces[spaceIndex] == i)
+        {
+            result[resIndex++] = ' ';
+            spaceIndex++;
+        }
+        result[resIndex++] = s[i];
+    }
+
+    result[resIndex] = '\0'; 
+    return result;
+}
